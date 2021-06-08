95bfbef6281b3989e5fe66a77fca19cc3b0c24c1
Mohammed Ziad <Mohammedziad599@gmail.com>
2021-06-08 21:48:27 +0300

    Adding install.sh
    this is a script that will first copy the Dynamic_Wallpapers folder to
    /usr/share/backgrounds/ and then will copy the xml files that are in the xml/ folder
    to /usr/share/gnome-background-properties to make gnome see the wallpapers and add them
    to gnome settings.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#bin/bash
+
+echo "installing the wallpepers to /usr/share/backgrounds/Dynamic_Wallpapers/"
+sudo cp -r ./Dynamic_Wallpapers/ /usr/share/backgrounds/
+sudo cp ./xml/* /usr/share/gnome-background-properties/
+echo "Wallpapers has been installed"
