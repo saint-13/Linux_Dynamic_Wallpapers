5cc99a32963e6babaf8e466e6bc76ac58967cc23
amrosolaiman <amroawad.soliman@mail.polimi.it>
2022-02-10 18:02:48 +0100

    :tada: Make a script for creating a dynamic wallpaper given just two wallpapers and a name for it.

---
+++ 1static2dynamic.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+read -p "Insert the path of the day wallpaper: " daywallpaper
+read -p "Insert the path of the night wallpaper: " nightwallpaper
+read -p "What name would like to attribute to the dynamic wallpaper? " dwallpapername
+
+daywallpapername=$(basename -- "$daywallpaper")
+daywallpaperextension="${daywallpapername##*.}"
+
+nightwallpapername=$(basename -- "$nightwallpaper")
+nightwallpaperextension="${nightwallpapername##*.}"
+
+mv -- "$daywallpaper" "${dwallpapername}-1.${daywallpaperextension}"
+
+mv -- "$nightwallpaper" "${dwallpapername}-2.${nightwallpaperextension}"
+
+mkdir Dynamic_Wallpapers/$dwallpapername
+
+mv -t Dynamic_Wallpapers/$dwallpapername "${dwallpapername}-1.${daywallpaperextension}" "${dwallpapername}-2.${nightwallpaperextension}"
+cd Dynamic_Wallpapers
+echo "<background>
+	<starttime>
+		<year>2018</year>
+		<month>1</month>
+		<day>1</day>
+		<hour>6</hour>
+		<minute>0</minute>
+		<second>0</second>
+	</starttime>
+
+	<static>
+		<file>/usr/share/backgrounds/Dynamic_Wallpapers/$dwallpapername/"${dwallpapername}-1.${daywallpaperextension}"</file>
+		<duration>42300.0</duration>
+	</static>
+
+	<transition type="overlay">
+		<duration>900.0</duration>
+		<from>/usr/share/backgrounds/Dynamic_Wallpapers/$dwallpapername/"${dwallpapername}-1.${daywallpaperextension}"</from>
+		<to>/usr/share/backgrounds/Dynamic_Wallpapers/$dwallpapername/"${dwallpapername}-2.${nightwallpaperextension}"</to>
+	</transition>
+
+	<static>
+		<file>/usr/share/backgrounds/Dynamic_Wallpapers/$dwallpapername/"${dwallpapername}-2.${nightwallpaperextension}"</file>
+		<duration>42300.0</duration>
+	</static>
+
+	<transition type="overlay">
+		<duration>900.0</duration>
+    <from>/usr/share/backgrounds/Dynamic_Wallpapers/$dwallpapername/"${dwallpapername}-2.${nightwallpaperextension}"</from>
+		<to>/usr/share/backgrounds/Dynamic_Wallpapers/$dwallpapername/"${dwallpapername}-1.${daywallpaperextension}"</to>
+	</transition>
+    </background>" > $dwallpapername.xml
+
+cd ..
+cd xml
+echo "<?xml version="1.0"?>
+<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
+<wallpapers>
+  <wallpaper deleted="false">
+    <name>$dwallpapername</name>
+    <filename>/usr/share/backgrounds/Dynamic_Wallpapers/$dwallpapername.xml</filename>
+    <options>zoom</options>
+    <shade_type>solid</shade_type>
+    <pcolor>#3465a4</pcolor>
+    <scolor>#000000</scolor>
+  </wallpaper>
+</wallpapers>" > $dwallpapername.xml
