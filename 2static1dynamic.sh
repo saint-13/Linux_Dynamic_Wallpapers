463c748458007077a6a9c7fd45b2417839831455
amrosolaiman <amroawad.soliman@mail.polimi.it>
2022-03-04 18:50:14 +0100

    :memo: md file tweaks

--- 2static1dynamic.sh
+++ 2static1dynamic.sh
@@ -4,8 +4,8 @@
 #it generates a compressed gif and the xml files for Gnome.
 #it commits the changes to the local git.
 
-read -p "Insert the path of the day wallpaper: " daywallpaper
-read -p "Insert the path of the night wallpaper: " nightwallpaper
+read -p "Insert the path of the day wallpaper: [full path or just file name if the file is in the same directory eg. DailyLove.jpg] " daywallpaper
+read -p "Insert the path of the night wallpaper: [full path or just file name if the file is in the same directory eg. NightlyLove.jpg]" nightwallpaper
 read -p "What name would like to attribute to the dynamic wallpaper? " dwallpapername
 echo "generating the gif file..."
 convert $daywallpaper $nightwallpaper -morph 10 -set delay 6 \( -clone 0 -set delay 100 \) -swap 0 +delete \( +clone   -set delay 100 \) +swap   +delete -loop 0 -duplicate 1,-2-1 $dwallpapername.gif
