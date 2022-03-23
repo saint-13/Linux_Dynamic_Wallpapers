c579010d7ec4610a35c9994744f534042b0e8bb5
saint-13 <68790180+saint-13@users.noreply.github.com>
2022-03-23 20:18:03 +0430

    Update Easy_Install.sh
    revert back to not delete gnome-background-properties

--- Easy_Install.sh
+++ Easy_Install.sh
@@ -11,12 +11,6 @@ then
 	echo "Setting up"
 fi
 
-if [[ -d /usr/share/gnome-background-properties ]]
-then 
-	sudo rm -r /usr/share/gnome-background-properties
-	sudo mkdir /usr/share/gnome-background-properties
-fi
-
 echo "Installing wallpapers..."
 sudo cp -r ./Dynamic_Wallpapers/ /usr/share/backgrounds/
 sudo cp ./xml/* /usr/share/gnome-background-properties/
