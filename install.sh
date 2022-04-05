373441fef2f08fdeab159511bb6b784993d442c7
mohammad <saint13@ymail.com>
2022-04-05 17:29:03 +0430

    :wrench: issue23

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ then
 fi
 
 echo "Installing wallpapers..."
-sudo cp -r ./Dynamic_Wallpapers/ /usr/share/backgrounds/
-sudo cp ./xml/* /usr/share/gnome-background-properties/
+sudo ln -s $(pwd)/Dynamic_Wallpapers /usr/share/backgrounds/Dynamic_Wallpapers
+sudo ln -s $(pwd)/xml/* /usr/share/gnome-background-properties/
 echo "Wallpapers has been installed. Enjoy setting them as your desktop background!"
