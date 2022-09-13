f6646ccf41a9519c7999df89de8f2676c2e60afe
saint-13 <68790180+saint-13@users.noreply.github.com>
2022-09-13 17:29:35 +0430

    Update install.sh
    fix install.sh problem

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ fi
 echo "Installing wallpapers..."
 sudo mkdir -p /usr/share/backgrounds/
 sudo mkdir -p /usr/share/gnome-background-properties/ 
-sudo ln -sf $(pwd)/Dynamic_Wallpapers /usr/share/backgrounds/Dynamic_Wallpapers
-sudo ln -sf $(pwd)/xml/* /usr/share/gnome-background-properties/
+sudo cp -r $(pwd)/Dynamic_Wallpapers /usr/share/backgrounds/Dynamic_Wallpapers
+sudo cp $(pwd)/xml/* /usr/share/gnome-background-properties/
 echo "Wallpapers has been installed. Enjoy setting them as your desktop background!"
